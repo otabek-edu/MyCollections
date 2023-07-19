d01e3f994d7cc28570f636d4d6d8ff0bcf23858e
Otabek Rustamov <otabek.pro@hotmail.com>
2023-07-19 23:50:26 +0500

    new api url

--- MyItems.Frontend/src/API/CONST.js
+++ MyItems.Frontend/src/API/CONST.js
@@ -1,4 +1,4 @@
 export default class CONST {
-  // static API_URL = "https://myitemswebapi.azurewebsites.net";
-  static API_URL = "http://localhost:5226";
+  static API_URL = "https://myitemswebapi.azurewebsites.net";
+  //static API_URL = "http://localhost:5226";
 }
